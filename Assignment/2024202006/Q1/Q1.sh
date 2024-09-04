e2fcf235b295bb970419540e036b03279328a3c8
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2024-09-04 17:24:43 +0530

    Delete Assignment/2024202006 directory

--- Assignment/2024202006/Q1/Q1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-awk 'NF' quotes.txt > quotes_empty.txt
-
-sort quotes.txt | uniq > quotes_rdup.txt
-
-awk -F '~' '{print $NF}' quotes.txt | sort | uniq -c | sort -nr > quotes_byperson.txt	
-
-awk '{for (i=1;i<=NF;i++) if ($i ~ /^s[^a]/) print $i}' quotes.txt > s_words.txt
